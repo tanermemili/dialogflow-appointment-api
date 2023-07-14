03cc7f8b92e11d66b0fd77f9d2ed60264863d45c
Taner Memili <Taner.Memili@web.de>
2023-07-14 16:47:47 +0200

    Added suggestions for user as part of the capacity reduction

--- dialogflow/src/main/java/com/dialogflow/application/service/ScheduleService.java
+++ dialogflow/src/main/java/com/dialogflow/application/service/ScheduleService.java
@@ -23,15 +23,40 @@ public class ScheduleService {
     }
 
     public String findTopTwoSchedules() {
-        String responseText = "";
-
         List<Schedule> schedules = scheduleRepository.findTopTwoSchedules();
 
-        int freeScheduleSlots = 0;
+        StringBuilder responseText = new StringBuilder("I would like to suggest you one of these " +
+                "dates, since we have a lot of capacity here: ");
 
         for (Schedule schedule : schedules) {
+            String eightString = "";
+            String eightThirtyString = "";
+            String nineString = "";
+            String fifteenString = "";
+            String fifteenThirtyString = "";
 
+            if (!schedule.getEight()) {
+                eightString = "08:00 - 08:30, ";
+            }
+            if (!schedule.getEightThirty()) {
+                eightThirtyString = "08:30 - 09:00, ";
+            }
+            if (!schedule.getNine()) {
+                nineString = "09:00 - 09:30, ";
+            }
+            if (!schedule.getFifteen()) {
+                fifteenString = "15:00 - 15:30, ";
+            }
+            if (!schedule.getFifteenThirty()) {
+                fifteenThirtyString = "15:30 - 16:00, ";
+            }
+
+            responseText.append(schedule.getWeekday() + ": " +
+                    eightString + eightThirtyString + nineString + fifteenString + fifteenThirtyString);
         }
-        return "";
+
+        responseText.append("which of these dates are feasible for you? Or do you prefer another day/time?");
+
+        return responseText.toString();
     }
 }
